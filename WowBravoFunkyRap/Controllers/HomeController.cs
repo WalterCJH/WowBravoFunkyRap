c470633419081ec52b8a9ffc2dd16faf3693a4d9
Walter <smickey33@livemail.tw>
2023-03-04 21:44:31 +0800

    加入專案檔案。

---
+++ WowBravoFunkyRap/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WowBravoFunkyRap.Models;
+
+namespace WowBravoFunkyRap.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet("/WOW_BRAVO_&_FUNKY_RAP.html")]
+        public IActionResult OldWebsite()
+        {
+            return RedirectToActionPermanent("Index", "Home");
+        }
+
+        [HttpGet("/WOW_BRAVO_%26_FUNKY_RAP.html")]
+        public IActionResult OldWebsite2()
+        {
+            return RedirectToActionPermanent("Index", "Home");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WowBravoFunkyRap/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿
+function _scroll() {
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop < 10) {
+        $(".navbar-opacity").css("opacity", 1);
+    }
+    else {
+        $(".navbar-opacity").css("opacity", 0.95);
+    }
+}
+$(window).on("scroll", function () {
+    _scroll()
+});;
